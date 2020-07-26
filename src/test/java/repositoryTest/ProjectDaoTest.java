f7bc411ff4d8bcde889c406f66acfb0924b29d35
Can Zhao <canzhao216@gmail.com>
2020-07-26 11:54:56 -0400

    update1

--- src/test/java/repositoryTest/ProjectDaoTest.java
+++ src/test/java/repositoryTest/ProjectDaoTest.java
@@ -1,5 +1,10 @@
 package repositoryTest;
 
+import com.psp.repository.ProjectDao;
+import com.psp.repository.ProjectDaoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ProjectDaoTest {
-    
+    @Autowired
+    private ProjectDao projectDao;
 }
